c1cd1353d3a7bebe456bc621c7c8d04fd9b16921
Yuzunoha <studysteadystandby@gmail.com>
2022-02-17 21:07:46 +0900

    動いた

--- html/laravelapp/database/migrations/2022_02_17_070754_create_manager_profiles_table.php
+++ html/laravelapp/database/migrations/2022_02_17_070754_create_manager_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateManagerProfileTable extends Migration
+class CreateManagerProfilesTable extends Migration
 {
     /**
      * Run the migrations.
