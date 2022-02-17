2b6b2d8b62bedde025942fd941e73e2c53a032c4
Yuzunoha <studysteadystandby@gmail.com>
2022-02-18 00:46:18 +0900

    実験

--- html/laravelapp/app/Http/Controllers/StudentProfileController.php
+++ html/laravelapp/app/Http/Controllers/StudentProfileController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StudentProfile;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class StudentProfileController extends BaseController
 {
     public function update()
     {
-        return 123;
+        $sp = StudentProfile::where('user_id', Auth::id())->first();
+        $sp->update(['student_profile_memo' => 'updated!!!']);
+
+        return [Auth::user()->studentProfile, $sp];
     }
 }
